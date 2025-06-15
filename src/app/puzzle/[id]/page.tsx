8af5dc25fdde5253b0adead9f0d8712c223a725a
LamPham123 <LamPham705@gmail.com>
2025-06-15 15:11:57 -0700

    final?

--- src/app/puzzle/[id]/page.tsx
+++ src/app/puzzle/[id]/page.tsx
@@ -21,10 +21,28 @@ const puzzles = [
     imageUrl: "/puzzle-images/NewOrleans.png"
   },
   {
+    riddle: "What did you buy to convince Dzanh to move to a new house and new elementary school??",
+    answer: "Xbox",
+    hint: "console device",
+    imageUrl: "/puzzle-images/Xbox.jpeg"
+  },
+  {
     riddle: "When is my birthday?",
     answer: "07/21/2005",
     hint: "xx/xx/xxxx",
     imageUrl: "/puzzle-images/Lam.png"
+  },
+  {
+    riddle: "Which boxer did your coworkers say you looked like a long  time ago?",
+    answer: "Manny Pacquiao",
+    hint: "",
+    imageUrl: "/puzzle-images/Manny.jpeg"
+  },
+  {
+    riddle: "What is Mom's favorite color??",
+    answer: "Purple",
+    hint: "",
+    imageUrl: "/puzzle-images/MomColor.png"
   },
   {
     riddle: "Where was this photo taken?",
@@ -33,10 +51,22 @@ const puzzles = [
     imageUrl: "/puzzle-images/Naples.png"
   },
   {
+    riddle: "Karate or Kung Fu?",
+    answer: "Kung Fu",
+    hint: "",
+    imageUrl: "/puzzle-images/Kung.jpeg"
+  },
+  {
     riddle: "Where am I working this summer?",
     answer: "Amazon",
     hint: "You should know this...",
     imageUrl: "/puzzle-images/Work.png"
+  },
+  {
+    riddle: "What stock did you sell to buy our house??",
+    answer: "Costco",
+    hint: "____ Stock",
+    imageUrl: "/puzzle-images/Stock.jpeg"
   },
   {
     riddle: "When is Mom's birthday?",
@@ -45,10 +75,10 @@ const puzzles = [
     imageUrl: "/puzzle-images/Mom.png"
   },
   {
-    riddle: "When is your birthday?",
-    answer: "11/29/1966",
-    hint: "xx/xx/xxxx",
-    imageUrl: "/puzzle-images/Dad.png"
+    riddle: "What kind of fish is Dory from Finding Nemo?",
+    answer: "Hippo Tang",
+    hint: "blue",
+    imageUrl: "/puzzle-images/Dory.webp"
   },
   {
     riddle: "What is your favorite quote right now?",
